ddd3b922046ad8fc50773a11721a8f25888aa5ae
Devon Chau <devonpis@gmail.com>
2025-05-09 11:51:06 +1000

    feat: update app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
 
   return (
     <div className="master-layout">
-      <a href="#header" id="skip-to-content">
+      <a href="#content" id="skip-to-content">
         skip to content
       </a>
       <h1 id="header">Atlassian Coding Interview Environment</h1>
       <p>Loaded time: {time}</p>
-      
+      <main id="content"></main>
     </div>
   );
 }
