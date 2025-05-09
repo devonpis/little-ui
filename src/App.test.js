8dac64cd606375aa79b08168a843ae86489a497e
Devon Chau <devonpis@gmail.com>
2025-05-09 12:18:44 +1000

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -37,16 +37,17 @@ afterEach(() => {
 beforeEach(() => {});
 afterAll(() => {});
 
-test("renders Atlassian heading and time", () => {
-  // useExampleHook.mockReturnValueOnce({});
-  render(<App />);
+describe("app", () => {
+  test("renders Atlassian heading and time", () => {
+    // useExampleHook.mockReturnValueOnce({});
+    render(<App />);
 
-  // Check the heading is present
-  const heading = screen.getByText(/Atlassian Coding Interview Environment/i);
-  expect(heading).toBeInTheDocument();
+    // Check the heading is present
+    const heading = screen.getByText(/Atlassian Coding Interview Environment/i);
+    expect(heading).toBeInTheDocument();
 
-  // Check the "Loaded at:" label is present
-  const timeText = screen.getByText(/Loaded time:/i);
-  expect(timeText).toBeInTheDocument();
+    // Check the "Loaded at:" label is present
+    const timeText = screen.getByText(/Loaded time:/i);
+    expect(timeText).toBeInTheDocument();
+  });
 });
-
